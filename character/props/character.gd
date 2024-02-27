a9e51d1712809410e5b6b50259c747101b7a2a92
zuiyu1998 <1542844298@qq.com>
2024-02-27 11:17:54 +0800

    refactor: 修改get_props

--- character/props/character.gd
+++ character/props/character.gd
@@ -8,21 +8,17 @@ var props: Props:
 
 var _props: Props = Props.new()
 
-func get_props(id: int) -> Props:
-	var tmp_props = Props.new()
-	
+func _get_props(id: int):
 	var model := Global.character_props_db.get_id(id)
-	tmp_props.initiative_id = model.initiative_id
+	_props.initiative_id = model.initiative_id
 	
 	for prop_value in model.prop_values:
 		var value = PropValue.new()
 		var prop_enum = PropEnum.get_prop_enum(prop_value["prop_enum"])
 		value.set_value(prop_value.value)
 		value.set_max(prop_value.max)
-		tmp_props.set_prop(prop_enum, value)
-		pass
-	
-	return tmp_props
+		_props.set_prop(prop_enum, value)
+		pass	
 
 func _init() -> void:
-	_props = get_props(props_meta.id)
+	_get_props(props_meta.id)
