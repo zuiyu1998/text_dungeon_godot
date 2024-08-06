422266e34d5ac68b3453f0c973d3ebc7ccd885ae
zuiyu1998 <1542844298@qq.com>
2024-08-06 19:46:46 +0800

    feat: 添加豁免判定

--- battle/battle_options.gd
+++ battle/battle_options.gd
@@ -8,9 +8,21 @@ var battting_first: int = 0
 var dodge_num: int = 0
 
 # 命中
-var hit_num: int = 0
+var physical_hit_num: int = 0
+
+# 命中
+var magic_hit_num: int = 0
+
+# 护甲
+var armor_num: int = 0
+
+# 豁免
+var immunity_num: int = 0
 
 func from_stats(stats: Stats) -> void:
 	battting_first = stats.storage.dnd_props.get_prop("battting_first").get_value()
 	dodge_num = stats.storage.dnd_props.get_prop("dodge").get_value()
-	hit_num = stats.storage.dnd_props.get_prop("physical_hit").get_value()
+	physical_hit_num = stats.storage.dnd_props.get_prop("physical_hit").get_value()
+	magic_hit_num = stats.storage.dnd_props.get_prop("magic_hit").get_value()
+	armor_num = stats.storage.dnd_props.get_prop("armor").get_value()
+	immunity_num = stats.storage.dnd_props.get_prop("immunity").get_value()
--- battle/battle_system.gd
+++ battle/battle_system.gd
@@ -29,11 +29,40 @@ static func get_batting_first(_stats_list: Array[Stats]) -> Array[BattleItem]:
 
 
 func battle():
+	var res = get_battle_result()
+
+
+func get_battle_result() -> BattleResult:
+	var res = BattleResult.new()
+	
 	var dodge_decision_res = get_dodge_decision()
+	res.dodge_decision_result = dodge_decision_res
 	
 	if not dodge_decision_res.success:
 		print("hit fail, dodge success")
-	pass
+	
+		var armor_decision_res = get_armor_decision()
+		res.armor_decision_result = armor_decision_res
+		
+		if not armor_decision_res.success:
+			print("hit fail, battle fail")
+			return res
+		
+	
+	return res
+	
+
+func get_armor_decision() -> DicisionResult:
+	var active_item := items[active_item_index]
+	var unactive_item := items[unactive_item_index]
+
+	var decision = Decision.new()
+	decision.base = active_item.options.physical_hit_num
+	decision.dc = unactive_item.options.dodge_num
+
+	var res = decision.decision()
+
+	return res
 
 
 func get_dodge_decision() -> DicisionResult:
@@ -41,7 +70,7 @@ func get_dodge_decision() -> DicisionResult:
 	var unactive_item := items[unactive_item_index]
 
 	var dodge_decision = Decision.new()
-	dodge_decision.base = active_item.options.hit_num
+	dodge_decision.base = active_item.options.physical_hit_num
 	dodge_decision.dc = unactive_item.options.dodge_num
 
 	var res = dodge_decision.decision()
@@ -54,3 +83,7 @@ func get_hit_result() -> void:
 class BattleItem:
 	var options: BattleOptions
 	var source: Stats
+
+class BattleResult:
+	var dodge_decision_result: DicisionResult
+	var armor_decision_result: DicisionResult
